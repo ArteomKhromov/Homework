63cae60cf5f3508d796f0c2004918b64fb448cfd
Arteom Khromov <khromovartem1997@gmail.com>
2021-09-26 00:23:46 +0300

    Readi work

---
+++ task_DEV_1/task_DEV_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task_DEV_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Enter A namber");
+           int index = int.Parse(Console.ReadLine());
+           if (index % 2 == 0 && index % 5 == 0)
+            {
+              Console.WriteLine("tutti-frutti");
+            }
+           else if (index % 2 == 0)
+            {
+              Console.WriteLine("tutti");
+            }
+           else if (index % 5 == 0)
+            {
+              Console.WriteLine("frutti");
+            } 
+           
+        }
+    }
+}
