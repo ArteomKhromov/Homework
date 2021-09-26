06544693addab5e49417bc57c6961032543009b5
Arteom Khromov <khromovartem1997@gmail.com>
2021-09-26 20:48:40 +0300

    third fixed indents

--- task_DEV_1/task_DEV_1/Program.cs
+++ task_DEV_1/task_DEV_1/Program.cs
@@ -6,25 +6,25 @@ namespace task_DEV_1
     {
         static void Main(string[] args)
         {
-             Console.WriteLine("Enter A number");
-             int inputNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter A number");
+            int inputNumber = int.Parse(Console.ReadLine());
            
-             if (inputNumber % 2 == 0 && inputNumber % 5 == 0)
-             {
-               Console.WriteLine("tutti-frutti");
-             }
-             else if (inputNumber % 2 == 0)
-             {
-               Console.WriteLine("tutti");
-             }
-             else if (inputNumber % 5 == 0)
-             {
-               Console.WriteLine("frutti");
-             } 
-             else
-             {
-               Console.WriteLine("This number is not divisible by 2 and not divisible by 5");
-             }
+            if (inputNumber % 2 == 0 && inputNumber % 5 == 0)
+            {
+            Console.WriteLine("tutti-frutti");
+            }
+            else if (inputNumber % 2 == 0)
+            {
+            Console.WriteLine("tutti");
+            }
+            else if (inputNumber % 5 == 0)
+            {
+            Console.WriteLine("frutti");
+            } 
+            else
+            {
+            Console.WriteLine("This number is not divisible by 2 and not divisible by 5");
+            }
         }
     }
 }
