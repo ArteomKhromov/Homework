c8b57d233128e893adaee3a15997058bd06cfae1
Arteom Khromov <khromovartem1997@gmail.com>
2021-09-26 19:43:39 +0300

    Bugs fixed

--- task_DEV_1/task_DEV_1/Program.cs
+++ task_DEV_1/task_DEV_1/Program.cs
@@ -6,21 +6,25 @@ namespace task_DEV_1
     {
         static void Main(string[] args)
         {
-          Console.WriteLine("Enter A namber");
-           int index = int.Parse(Console.ReadLine());
-           if (index % 2 == 0 && index % 5 == 0)
-            {
-              Console.WriteLine("tutti-frutti");
-            }
-           else if (index % 2 == 0)
-            {
-              Console.WriteLine("tutti");
-            }
-           else if (index % 5 == 0)
-            {
-              Console.WriteLine("frutti");
-            } 
+          Console.WriteLine("Enter A number");
+           int inputNumber = int.Parse(Console.ReadLine());
            
+            if (inputNumber % 2 == 0 && inputNumber % 5 == 0)
+            {
+                Console.WriteLine("tutti-frutti");
+            }
+            else if (inputNumber % 2 == 0)
+            {
+                Console.WriteLine("tutti");
+            }
+            else if (inputNumber % 5 == 0)
+            {
+                Console.WriteLine("frutti");
+            } 
+            else
+            {
+                Console.WriteLine("This number is not divisible by 2 and not divisible by 5");
+            }
         }
     }
 }
