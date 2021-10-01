044bb82d7951cf27dd88d692eac46a930f71c63d
Arteom Khromov <khromovartem1997@gmail.com>
2021-10-01 20:54:18 +0300

    added variable i

--- task_DEV_1/task_DEV_1/Program.cs
+++ task_DEV_1/task_DEV_1/Program.cs
@@ -6,28 +6,28 @@ namespace task_DEV_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter A number");
+            Console.WriteLine("Enter the first number");
             int firstNumber = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter B number");
+            Console.WriteLine("Enter the second number");
             int lastNumber = int.Parse(Console.ReadLine());
 
-            for (; firstNumber <= lastNumber; firstNumber++)
+            for (int i = firstNumber ; i <= lastNumber ; i++)
             {
-                if (firstNumber % 2 == 0 && firstNumber % 5 == 0)
+                if (i % 2 == 0 && i % 5 == 0)
                 {
                     Console.WriteLine("tutti-frutti");
                 }
-                else if (firstNumber % 2 == 0)
+                else if ( i % 2 == 0)
                 {
                     Console.WriteLine("tutti");
                 }
-                else if (firstNumber % 5 == 0)
+                else if ( i % 5 == 0)
                 {
                     Console.WriteLine("frutti");
                 }
                 else
                 {
-                    Console.WriteLine($"Number {firstNumber} can’t be divided on 2 or 5");
+                    Console.WriteLine($"Number {i} can’t be divided on 2 or 5");
                 }
             }
         }
