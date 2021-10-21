ef9519dd06ceb836f95a99cf5820ea9c3ec8f7c3
Arteom Khromov <khromovartem1997@gmail.com>
2021-10-21 20:15:19 +0300

    The break operator has been removed

--- task_DEV_2/task_DEV_2/Program.cs
+++ task_DEV_2/task_DEV_2/Program.cs
@@ -9,47 +9,47 @@ namespace task_DEV_2
             Console.WriteLine("Enter the size of the array :");
             int arrayLength = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i < arrayLength;i++)
+            if (arrayLength <= 20)
             {
-                if (arrayLength <= 20) 
-                    
-                    break;
-                
+                int[] numberArray = new int[arrayLength];
+
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    Console.WriteLine($"Enter an array element № {i}");
+                    numberArray[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                int max = numberArray[0];
+                int min = numberArray[0];
+                int minIndex = 0;
+                int maxIndex = 0;
+
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    if (numberArray[i] > max)
+                    {
+                        max = numberArray[i];
+                        maxIndex = i;
+                    }
+                    if (numberArray[i] < min)
+                    {
+                        min = numberArray[i];
+                        minIndex = i;
+                    }
+                    Console.WriteLine(numberArray[i]);
+                }
+                numberArray[maxIndex] = min;
+                numberArray[minIndex] = max;
+
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    Console.WriteLine(numberArray[i]);
+                }
+            }
+            else
+            {
                 Console.WriteLine("array cannot be <= 20");
             }
-            int[] numberArray = new int[arrayLength];
 
-            for (int i = 0; i < arrayLength;i++)
-            {
-                Console.WriteLine($"Enter an array element № {i}");
-                numberArray[i] = Convert.ToInt32(Console.ReadLine());
-            }
-            int max = numberArray[0];
-            int min = numberArray[0];
-            int minIndex = 0;
-            int maxIndex = 0;
-
-            for (int i = 0; i < arrayLength; i++)
-            {
-                if (numberArray[i] > max)
-                {
-                    max = numberArray[i];
-                    maxIndex = i;
-                }
-                if (numberArray[i] < min)
-                {
-                    min = numberArray[i];
-                    minIndex = i;
-                }
-                Console.WriteLine(numberArray[i]);
-            }
-            numberArray[maxIndex] = min;
-            numberArray[minIndex] = max;
-
-            for (int i = 0; i < arrayLength; i++)
-            {
-                Console.WriteLine(numberArray[i]);
-            }
         }
     }
 }
