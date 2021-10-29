6a771ff490ec8a1bcb33281841fed44fbe7aae94
Arteom Khromov <khromovartem1997@gmail.com>
2021-10-29 18:01:20 +0300

    Finding a Prime Number

---
+++ task_DEV_3/task_DEV_3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace task_DEV_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of the array :");
+            int arrayLength = Convert.ToInt32(Console.ReadLine());
+
+            if (arrayLength <= 5)
+            {
+                int[] numberArray = new int[arrayLength];
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    Console.WriteLine($"Enter an array element № {i}");
+                    numberArray[i] = Convert.ToInt32(Console.ReadLine());
+                    if (numberArray[i] < 0)
+                    {
+                        Console.WriteLine("This is a negative number");
+                        break;
+                    }
+                }
+                bool isPrimeNumber = true;
+
+
+
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    if (numberArray[i] == 1 || numberArray[i] == 2)
+                    {
+                        Console.WriteLine($"{numberArray[i]} is prime number");
+                    }
+                    for (int j = 2; j < numberArray[i]; j++)
+                    {
+                        if (numberArray[i] % j == 0)
+                        {
+                            isPrimeNumber = false;
+                            Console.WriteLine($"{numberArray[i]} is not prime number");
+                            break;
+                        }
+                        else
+                        {                            
+                            Console.WriteLine($"{numberArray[i]} is prime number");
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Array has exceeded the allowed value");
+            }
+        }
+    }
+}
