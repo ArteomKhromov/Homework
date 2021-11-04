b9fcbc865f013203cf3b50e7299410599ee75163
Arteom Khromov <khromovartem1997@gmail.com>
2021-11-04 22:04:16 +0300

    Methods renamed, code style corrected.

--- task_DEV_2/task_DEV_2/Program.cs
+++ task_DEV_2/task_DEV_2/Program.cs
@@ -4,15 +4,36 @@ namespace task_DEV_2
 {
     class Program
     {
-        static void InputArrayElements(int[] inputNumbers, int arrayLength)
+        static void Main(string[] args)
         {
+            Console.WriteLine("Enter the length of the array :");
+            int arrayLength = Convert.ToInt32(Console.ReadLine());
+
+            if (arrayLength <= 20)
+            {
+                int[] inputNumbers = CreateNumbersArray(arrayLength);
+                PrintNumberArraryToConsole(inputNumbers, arrayLength);
+                SwapMinMaxElements(inputNumbers, arrayLength);
+                PrintNumberArraryToConsole(inputNumbers, arrayLength);
+            }
+            else
+            {
+                Console.WriteLine("array cannot be > 20");
+            }
+        }
+
+        static int CreateNumbersArray(int arrayLength)
+        {
+            int[] inputNumbers = new int[arrayLength];
             for (int i = 0; i < arrayLength; i++)
             {
                 Console.WriteLine($"Enter an array element № {i}");
                 inputNumbers[i] = Convert.ToInt32(Console.ReadLine());
             }
+            return inputNumbers;
         }
-        static void ChangeMinMaxElements(int[] inputNumbers, int arrayLength)
+
+        static int[] SwapMinMaxElements(int[] inputNumbers, int arrayLength)
         {
             int max = inputNumbers[0];
             int min = inputNumbers[0];
@@ -35,30 +56,15 @@ namespace task_DEV_2
             }
             inputNumbers[maxIndex] = min;
             inputNumbers[minIndex] = max;
+
+            return inputNumbers[minIndex];
         }
-        static void OutputArray(int[] inputNumbers, int arrayLength)
+
+        static void PrintNumberArraryToConsole(int[] inputNumbers, int arrayLength)
         {
             for (int i = 0; i < arrayLength; i++)
             {
                 Console.WriteLine(inputNumbers[i]);
-            }
-        }
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Enter the length of the array :");
-            int arrayLength = Convert.ToInt32(Console.ReadLine());
-
-            if (arrayLength <= 20)
-            {
-                int[] inputNumbers = new int[arrayLength];
-
-                InputArrayElements(inputNumbers, arrayLength);
-                ChangeMinMaxElements(inputNumbers, arrayLength);
-                OutputArray(inputNumbers, arrayLength);
-            }
-            else
-            {
-                Console.WriteLine("array cannot be > 20");
             }
         }
     }
