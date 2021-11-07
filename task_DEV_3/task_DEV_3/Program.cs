4ea6d4d51c4adf2040ae59c886725a9b0f5541de
Arteom Khromov <khromovartem1997@gmail.com>
2021-11-07 14:41:41 +0300

    Maine method moved to the beginning

--- task_DEV_3/task_DEV_3/Program.cs
+++ task_DEV_3/task_DEV_3/Program.cs
@@ -4,38 +4,6 @@ namespace task_DEV_3
 {
     class Program
     {
-        static void InputOfArrayElements(int[] numberArray, int arrayLength)
-        {
-            for (int i = 0; i < arrayLength; i++)
-            {
-                Console.WriteLine($"Enter an array element № {i}");
-                numberArray[i] = Convert.ToInt32(Console.ReadLine());
-            }
-        }
-        static void ExaminationNumberArray(int[] numberArray, int arrayLength)
-        {
-            bool isPrimeNumber;
-            for (int i = 0; i < arrayLength; i++)
-            {
-                isPrimeNumber = true;
-                for (int j = 2; j < numberArray[i]; j++)
-                {
-                    if (numberArray[i] % j == 0)
-                    {
-                        isPrimeNumber = false;
-                        break;
-                    }
-                }
-                if (isPrimeNumber)
-                {
-                    Console.WriteLine($"{ numberArray[i]} is prime numbr");
-                }
-                else
-                {
-                    Console.WriteLine($"{ numberArray[i]} is not prime number");
-                }
-            }
-        }
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the length of the array :");
@@ -43,15 +11,49 @@ namespace task_DEV_3
 
             if (arrayLength <= 5)
             {
-                int[] numberArray = new int[arrayLength];
-
-                InputOfArrayElements(numberArray, arrayLength);
-                ExaminationNumberArray(numberArray, arrayLength);
+                int[] numbersArray = CreateNumbersArray(arrayLength);
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    if (CheckNumbersForPrime(numbersArray[i]))
+                    {
+                        Console.WriteLine($"{ numbersArray[i]} is prime numbr");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{ numbersArray[i]} is not prime number");
+                    }
+                }
             }
             else
             {
                 Console.WriteLine("Array length cannot exceed 5");
             }
+        }
+
+        static int[] CreateNumbersArray(int arrayLength)
+        {
+            int[] numbersArray = new int[arrayLength];
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.WriteLine($"Enter an array element № {i}");
+                numbersArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            return numbersArray;
+        }
+
+        static bool CheckNumbersForPrime(int number)
+        {
+            bool isPrimeNumber = true;
+
+            for (int j = 2; j < number; j++)
+            {
+                if (number % j == 0)
+                {
+                    isPrimeNumber = false;
+                    break;
+                }
+            }
+            return (isPrimeNumber);
         }
     }
 }
