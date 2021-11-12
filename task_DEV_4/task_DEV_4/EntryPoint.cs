d45cc35fdd222a11a3d66b9c01945a171575fc21
Arteom Khromov <khromovartem1997@gmail.com>
2021-11-12 21:45:54 +0300

    Creating a class rectangle

---
+++ task_DEV_4/task_DEV_4/EntryPoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace task_DEV_4
+{
+
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {            
+            Rectangle rectangle = new Rectangle();
+            int height = 5;
+            int width = 6;
+            rectangle.SettingRectangleValues(height, width);            
+            int rectanglePerimetr = rectangle.CalculatingThePerimeterOfARectangle();
+            Console.WriteLine($"The perimetr of a rectangle is {rectanglePerimetr}");
+            int rectangleArea = rectangle.CalculatingTheAreaOfARectangle();
+            Console.WriteLine($"The area of a rectangle is {rectangleArea}");
+            bool isSquare = rectangle.CheckForASquare();
+            if (isSquare)
+            {
+                Console.WriteLine("Square");
+            }
+            else
+            {
+                Console.WriteLine("Rectangle");
+            }
+            
+        }
+    }
+}
---
+++ task_DEV_4/task_DEV_4/Rectangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_DEV_4
+{
+    class Rectangle
+    {
+
+        public int height;
+        public int width;
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(int height, int width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+        public void SettingRectangleValues(int height, int width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+        public int CalculatingThePerimeterOfARectangle()
+        {
+            return (height + width) * 2;
+        }
+
+        public int CalculatingTheAreaOfARectangle()
+        {
+            return height * width;
+        }
+
+        public bool CheckForASquare()
+        {
+            bool isSquare = false;
+            if (height == width)
+            {
+                isSquare = true;                
+            }            
+            return isSquare; ;
+        }
+    }
+}
