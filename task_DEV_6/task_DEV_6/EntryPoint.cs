558e705eab1b8a485c59549fd25aa9e9972ddf27
Arteom Khromov <khromovartem1997@gmail.com>
2022-02-23 20:18:14 +0300

    Stack Based Arrays.

---
+++ task_DEV_6/task_DEV_6/EntryPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task_DEV_6
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Stack stack = new Stack();
+                stack.Push(10);
+                int count = stack.Count();
+                int pop = stack.Pop();                
+                bool empty = stack.IsEmpty();
+                int capacity = stack.Capacity();
+            }
+            catch (IndexOutOfRangeException i)
+            {
+                Console.WriteLine(i.Message);
+            }
+        }
+    }
+}
---
+++ task_DEV_6/task_DEV_6/Stack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace task_DEV_6
+{
+    class Stack
+    {        
+        public int[] stack;
+        public int count;
+
+        public Stack()
+        {
+            stack = new int[10];
+        }
+        public Stack(int number)
+        {
+            stack = new int[number];
+        }
+
+        public int Capacity()
+        {
+            return stack.Length;
+        }
+
+        public int Count()
+        {
+            return count;
+        }
+
+        public bool IsEmpty()
+        {
+            if (count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void NegativeResize()
+        {
+            if (count == stack.Length / 2)
+            {
+                Array.Resize(ref stack, stack.Length / 2);
+            }
+        }
+
+        public int Pop()
+        {
+            NegativeResize();
+            return stack[--count];
+        }
+
+        public void PositiveResize()
+        {
+            if (count == stack.Length)
+            {
+                Array.Resize(ref stack, stack.Length * 2);
+            }
+        }
+
+        public int Push(int enteredNumber)
+        {
+            PositiveResize();
+            return stack[count++] = enteredNumber;
+        }
+    }
+}
